fefd8241b0c75c5bbd70d82e5729baafceaafc1e
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-03-12 12:51:06 +0400

    js into

---
+++ JSLessons/lesson1-into/script.js
@@ -0,0 +1,20 @@
+/*
+more line
+*/
+//one line
+// document.write("Hello1");
+// debugger;   
+// console.log("Hello1");
+// console.log("Hello2");
+// console.loga("Hello3");
+// console.log("Hello4");
+// console.log("Hello5");
+
+// const text = document.querySelector('p');
+// const btn = document.querySelector('button');
+
+// const capital =()=>{
+//     text.innerHTML="Baku";
+// }
+
+// btn.addEventListener('click', capital);

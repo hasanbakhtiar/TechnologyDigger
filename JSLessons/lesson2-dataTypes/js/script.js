0b3f49f23f8947c6cd48453a721894c482ee91a7
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-03-12 13:18:51 +0400

    js datatype

---
+++ JSLessons/lesson2-dataTypes/js/script.js
@@ -0,0 +1,38 @@
+// Primitive (Bəsit)  Data(Data) Type(Tipi) 
+//  string
+const stringInfo = "Hello";
+console.log(stringInfo);
+
+//  number
+const infoNumberOne = 5;
+const infoNumberTwo = 15;
+console.log(infoNumberOne+infoNumberTwo);
+
+//  boolean
+const booleanInfo1 = true;
+const booleanInfo2 = false;
+console.log(booleanInfo2);
+
+// Composite(Birləşik) Data(Data) Type(Tipi) 
+//  array
+const infoArray = ["Hello",5,true];
+console.log(infoArray);
+
+//  object
+const infoObj = {
+    name: "Ferhad",
+    surname:"Qayibov",
+    age: 20,
+    army:false
+
+
+}
+console.log(infoObj.name);
+console.log(infoObj.surname);
+
+// Special(Xüsusi)     Data(Data) Type(Tipi) 
+//  undefined
+const a = undefined;
+console.log(a);
+//  null
+console.log(null);

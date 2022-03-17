797cb029f309384bcccd8192548189c8a8e198b0
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-03-17 16:57:41 +0400

    operators

---
+++ JSLessons/lesson3-oprators/js/script.js
@@ -0,0 +1,91 @@
+// OPERATORS 
+// Assingment operators - Daxil etmə opertorları
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+// Arithmetic operators - Riyazi opertorlar
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+
+// Comparison operators - Müqayisə(Qarşılaşdırma) opertorları 
+// ==
+// ===
+// <
+// >
+// <=
+// >=
+// != 
+// !== 
+
+// Logical operators - Məntiqi operatorlar
+// && - AND
+// || - OR
+// ! - NOT
+
+
+// if (((10<1) && (5<10)) || true) {
+//     console.log("Bu düzdür");
+// }
+// else{
+//     console.log("Bu Səhvdir");
+// }
+
+
+let info ="Hello " ;
+info+= "JavaScript ";
+
+console.log(info);
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+const a = 10;
+const b = 5;
+
+
+
+// (5>10) ? console.log(true):console.log(false);;
+
+
+
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+
+// console.log(void(aJ));
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2)(ternary)
+// 5>10 ? console.log(true): console.log(false);
+// (5 >= 10) ? document.write("Hi") : document.write("Bye");
+// şərt            ? true                 : false
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// delete       (objectin dəyərini silmək üçündür)
+// new          (yeni dəyər yaratməq üçündür)
+// this         (mövcud dəyəri işarələmək üçündür)
+// in           (dəyişkənin(object) daxilində dəyərin olub olmamasını yoxlamaq üçündür)
+// instanceof   (dəyişkənin(objectin xüsusiyyətinin) içində xüsusiyyət olub olmamasını yoxlamaq üçündür)
+
+
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------

1407d6501d3e4180a748a454ec8a6e6637dc7e85
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-07-28 12:48:58 +0400

    typescript types

---
+++ JSLessons/TypeScript/lesson2-object-union-literal-custom-types/sandbox.js
@@ -0,0 +1,54 @@
+// let infoA = 5;
+// // type safe 
+// let info:number = 5;
+// console.log(info);
+// function a(data:string){
+//     console.log(data);
+//  }
+// a("Hello");
+// let infoObj:{
+//     name:string,
+//     surname:string,
+//     age:number,
+//     education:boolean,
+//     drive:any
+// }
+//  infoObj={
+//     name:"Ferhad",
+//     surname:"Qayibov",
+//     age:18,
+//     education:true,
+//     drive:(carname:string)=>{
+//         return `${infoObj.name} + ${carname}`
+//     }
+// }
+// infoObj={
+//     name:"Ferhad",
+//     surname:"Qayibov",
+//     age:18,
+//     education:true,
+//     drive:function(carname:string){
+//         return `${infoObj.name} + ${carname}`
+//     }
+// }
+// infoObj={
+//     name:"Ferhad",
+//     surname:"Qayibov",
+//     age:18,
+//     education:true,
+//     drive(carname:string){
+//         return `${infoObj.name} + ${carname}`
+//     }
+// }
+// console.log(infoObj.name);
+// console.log(infoObj.drive("BMW"));
+// let car:string = "red";
+// let colorCar = function(color:"blue" | "green" | 'red'):void{
+//         car = color;
+// }
+// colorCar("blue")
+// console.log(car);
+// let info : "google" | "react" | "BMW";
+// info="react";
+var a = 5;
+console.log(a);

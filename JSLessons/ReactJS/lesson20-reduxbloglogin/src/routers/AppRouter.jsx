480ea89f9b9beecd4b4178d0b7ed88e1ef1f5808
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-09-02 11:58:58 +0400

    router 5

--- JSLessons/ReactJS/lesson20-reduxbloglogin/src/routers/AppRouter.jsx
+++ JSLessons/ReactJS/lesson20-reduxbloglogin/src/routers/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Nav from "../components/Nav";
 import AddBlogPage from "../components/pages/addeditremoveform/AddBlogPage";
 import EditBlogPage from "../components/pages/addeditremoveform/EditBlogPage";
@@ -15,16 +15,16 @@ const AppRouter = () => {
     <BrowserRouter>
       <Nav />
       <div className="container mt-5">
-        <Routes>
-          <Route path="/"  element={<HomePage />}></Route>
-          <Route path="/blog"  element={<BlogListPage />}></Route>
-          <Route path="/blog/:id" element={<BlogDetails />}></Route>
-          <Route path="/addblog" element={<AddBlogPage />}></Route>
-          <Route path="/edit/:id" element={<EditBlogPage />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
-          <Route path="/admin" element={<Login />}></Route>
-          <Route path="*" element={<NotFoundPage/>}></Route>
-        </Routes>
+        <Switch>
+          <Route path="/" exact component={HomePage }></Route>
+          <Route path="/blog" exact  component={BlogListPage }></Route>
+          <Route path="/blog/:id" component={BlogDetails }></Route>
+          <Route path="/addblog" component={AddBlogPage }></Route>
+          <Route path="/edit/:id" component={EditBlogPage }></Route>
+          <Route path="/dashboard" component={Dashboard }></Route>
+          <Route path="/admin" component={Login }></Route>
+          <Route  component={NotFoundPage}></Route>
+          </Switch>
       </div>
     </BrowserRouter>
   );
